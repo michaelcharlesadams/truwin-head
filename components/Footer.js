cf758ff254d4a665da7d38cf08334cafef9a7fde
Michael Adams <mikea0606@gmail.com>
2022-12-07 16:56:34 -0600

    added site logo sub footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ import Link from "next/link";
 function Footer() {
     return (
        <>
-       <div id="footer-section" className="mx-auto">
+       <div id="footer-section" className="mx-auto">    
+        
             <style jsx> {`
                 #footer-content p {
                     padding-bottom: 8px;
@@ -13,14 +14,25 @@ function Footer() {
             `}
                
             </style>
-            <div id="footer-wrapper" className="px-5 mt-20 bg-truwinblue-900 text-white lg:flex lg:flex-wrap">
+            <div id="sub-footer-wrapper" className="p-10 mt-20 bg-truwinsoftblue-primary">
+
+                <div className="grid grid-cols-1 sx:grid-cols-2 md:grid-cols-4 ">
+                    <img className="m-4 mx-auto" src="/images/bbb.png" width='140px' height='54px' />
+                    <img className="m-4 mx-auto" src="/images/lake-houston-chamber.png" width='165px' height='59px' />
+                    <img className="m-4 mx-auto" src="/images/houzz.png" width='165px' height='44px'/>
+                    <img className="m-4 mx-auto"  src="/images/google.png" width='131px' height='58px' />
+                </div>
+               
+            </div>
+            
+            <div id="footer-wrapper" className="px-5 bg-truwinblue-900 text-white lg:flex lg:flex-wrap">
                 <div id="footer-header" className="text-center w-36 mx-auto py-16 lg:w-2/5">
                     <img src="/images/truwin-w-logo.png" className="lg:mx-auto py-2" alt="Truwin logo" />
                     <p className="text-[11px]">THE EXTERIOR EXPERTS</p>
                     <div id="social-footer" className="flex w-auto mx-auto justify-evenly mt-10 lg:hidden">
                         <span className="w-6"><Image 
-                         width={46}
-                         height={46}
+                        width={46}
+                        height={46}
                         src="/images/Facebook - truwin.png" 
                         alt="Facebook logp" />
                         </span>
@@ -39,7 +51,7 @@ function Footer() {
                         src="/images/instagram - truwin.png" 
                         alt="Instagram Logo" /></span>
                     </div>
-            </div>
+                </div>
     
             <div id="footer-content" className="flex flex-wrap mx-auto text-xs w-11/12 pl-[10%] sm:flex-nowrap sm:pl-0 lg:w-3/5 lg:pt-12">
                 <div id="footer-col-1" className="w-2/4 mb-12 lg:mb-5">
@@ -89,8 +101,8 @@ function Footer() {
                     <div id="social-footer" className="w-2/5 mx-auto justify-evenly hidden lg:flex">
                         
                     <span className="w-6"><Image 
-                         width={46}
-                         height={46}
+                        width={46}
+                        height={46}
                         src="/images/Facebook - truwin.png" 
                         alt="Facebook logp" />
                         </span>
@@ -108,10 +120,10 @@ function Footer() {
                         height={46}
                         src="/images/instagram - truwin.png" 
                         alt="Instagram Logo" /></span>
-
+                        </div>
                     </div>
-                </div>
             </div>
+            
         </div>
         {/**  START OF SCRIPTS */}
        
