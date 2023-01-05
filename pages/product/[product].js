6d7fa8dc7d6abb445ac7eb008f7534bd72fe210f
Michael Adams <mikea0606@gmail.com>
2023-01-05 16:33:38 -0600

    add styling update to product page

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -77,7 +77,7 @@ export default function Product({post, morePost}) {
                     
                     return (
                         
-                        <div key={index} className="w-auto border rounded border-truwinblue-300 px-8 pt-8 mt-4 sm:mt-4 max-w-[400px] relative min-h-[350px]" >
+                        <div key={index} className="relative w-auto border rounded border-truwinblue-300 px-8 pt-8 mt-4 sm:mt-4 max-w-[400px] min-h-[390px] sx:min-h-[350px] md:min-h-[320px]">
                           <Link href={material.uri}>
                              <a><img src='/images/arrow-circle.png' className='absolute top-8 right-4 w-6 h-6'/></a>
                           </Link>
@@ -85,12 +85,11 @@ export default function Product({post, morePost}) {
                             <p className="text-2xl font-graphikSemibold text-truwinblue-900">
                                 <Link href={material.uri}><a>{material.title}</a></Link>
                               </p>
-                            <p className="text-sm font-graphik pt-4">{material.product_Materials.materialBlurp}</p>
+                            <p className="text-sm font-[Cigars]  text-truwinblue-900 pt-4">{material.product_Materials.materialBlurp}</p>
                            <div className="w-auto h-auto mx-auto absolute px-5 bottom-[-5px] left-[15%] sx:left-[5%]">
                                 <Image 
                                     src={material.product_Materials.materialImage.sourceUrl}
                                     width={material.product_Materials.materialImage.mediaDetails.width}
-                                  
                                     height={material.product_Materials.materialImage.mediaDetails.height} 
                                    
                                 />
