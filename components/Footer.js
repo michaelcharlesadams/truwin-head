10e64dcc924e6931a9d213ea0f50adbb80ee7c2b
Michael Adams <mikea0606@gmail.com>
2023-02-10 16:07:31 -0600

    remove roofing from footer

--- components/Footer.js
+++ components/Footer.js
@@ -89,7 +89,7 @@ function Footer() {
                     <p>
                     <Link href="/product/siding"><a >Siding</a></Link>
                     </p>
-                    <p><Link href="/product/roofing"><a >Roofing</a></Link></p>
+                    
                 </div>
                 <div id="footer-col-2" className="w-2/4 mb-12 lg:mb-5">
                     <h4 className="font-graphikSemibold text-truwinblue-300  pb-4">About Truwin</h4>     
