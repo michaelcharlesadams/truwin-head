d35e5caac7222d155bd00545f6a7873dfd430127
Michael Adams <mikea0606@gmail.com>
2023-02-14 13:04:40 -0600

    added google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -20,6 +20,14 @@ function Layout({ children }) {
         <title>Truwin Windows, Doors, & Siding</title>
         <link rel="icon" href="/truwin-favicon-blue.png" />
         <meta name="description" content="We are the transparent windows, doors, and siding company. Offering better windows and doors throughout the Houston and surrounding metropolitan area."></meta>
+        <script key="ga_script" async src="https://www.googletagmanager.com/gtag/js?id=UA-125406422-1"></script>
+
+        <script key="ga_init" dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-125406422-1');
+        `}} />
       </Head>
       <Header />
       <Nav />
