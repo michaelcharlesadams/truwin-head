c399f3851659dc59016f6460c5062d07184e2b25
Michael Adams <mikea0606@gmail.com>
2023-04-20 14:00:52 -0500

    add roofing  meta description

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -23,23 +23,7 @@ export default function Product({post, morePost}) {
   const contentCard2 = productPage_2.productProductServicesCard2;
 
   //console.log(vendorIcons)
-  let descriptionToggle = "" 
-  switch(slug) {
-     case "windows":
-       descriptionToggle = "Specializing in premier Houston, TX windows replacement services. Truwin is your transparent windows company servicing all of Houston & surrounding areas.";
-       break;
-     case "doors":
-       descriptionToggle = "Truwin is your experienced Houston, TX replacement doors contractors. We offer the best brands made in the US at transparent prices. Call Us! 832-777-3681";
-       break;
-     case "siding":
-       descriptionToggle = "Whole home siding replacement at transparent prices.We're proud to provide Houston TX siding services. Replace your siding with us! 832-777-3681";
-       break;
-    case "roofing":
-        descriptionToggle = "Roofing replacement at transparent prices. We're proud to provide Houston TX roofing services. Replace your roofing with us! 832-777-3681";
-        break;
-     default:
-       descriptionToggle = "We are the transparent windows, doors, and siding company. Offering better windows and doors throughout the Houston and surrounding metropolitan area.";
-   }
+
 
   return (
     <>
@@ -53,6 +37,8 @@ export default function Product({post, morePost}) {
           ? "Truwin is your experienced Houston, TX replacement doors contractors. We offer the best brands made in the US at transparent prices. Call Us! 832-777-3681"
           : slug === "siding"
           ? "Whole home siding replacement at transparent prices.We're proud to provide Houston TX siding services. Replace your siding with us! 832-777-3681" 
+          : slug === "roofing"
+          ? "Roofing replacement at transparent prices. We're proud to provide Houston TX roofing services. Replace your roofing with us! 832-777-3681" 
           : "We are the transparent windows, doors, and siding company. Offering better windows and doors throughout the Houston and surrounding metropolitan area."
         }
         ></meta>
